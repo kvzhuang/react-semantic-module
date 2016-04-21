b79c79baa12b4f7764ad5fb1d36308c8cd70bba8
DerekHung <ccumusical@gmail.com>
2016-04-21 13:57:49 +0800

    module setting

---
+++ index.js
@@ -0,0 +1,10 @@
+import LightBox from 'client/components/lightbox';
+import Button from 'client/components/button';
+import DropdownMenu from 'client/components/dropdowMenu';
+import DropList from 'client/components/dropList';
+import RadioGroup from 'client/components/radioGroup';
+import TextFeild from 'client/components/textfeild';
+
+import Validator from 'util/validator';
+
+export { LightBox, Button, DropdownMenu, DropList, RadioGroup, TextFeild, Validator };
