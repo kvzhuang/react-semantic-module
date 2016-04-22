440923df92512f2a5ffed6cf316a9b2c9b6fc147
DerekHung <ccumusical@gmail.com>
2016-04-22 12:00:31 +0800

    no message

--- client/components/dropdownMenu/index.js
+++ client/components/dropdownMenu/index.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import style from './style.css';
-import List from 'client/components/dropdownMenu/list';
+import List from './list';
 import ReactDOM from 'react-dom';
 import Target from './target';
 
